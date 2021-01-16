6b437c4fac4bb3fb6e278fde952d8815de1b0ebe
Aleksandra Ivanets <aleksandraivanets@iMac-Aleksandra.local>
2021-01-16 21:45:32 +0200

    homework_JS-01

---
+++ homework_JS-01/js/script.js
@@ -0,0 +1,63 @@
+//String
+const str = 'Привіт';
+
+str1 = 'Привіт';
+
+console.log(str)
+//Number
+let num = 123;
+
+num = 123;
+
+console.log(123)
+//Boolean
+let flag = true;
+
+console.log(flag)
+
+const txt = 'true';
+
+txt1 = 'true';
+
+console.log(true)
+
+// part-2
+
+let bookPage = 200;
+
+console.log(bookPage * 11)
+
+
+let isDetective = false;
+
+let bookName = 'Emma'
+
+console.log('Наші книги: Назва: ' + bookName + ', сторінки: ' + bookPage + '. Це детектив? ' + isDetective);
+
+let leng = bookName.length;
+
+console.log('Довжина назви = ' + leng);
+
+let hobby = 'Письменник';
+
+let hobby2 = 45;
+
+console.log(hobby)
+
+
+
+const str2 = 'IT123';
+const numStr = '15';
+
+console.log(typeof numStr)
+
+const msg = 'HI reader'
+
+console.log(msg);
+
+const message = 'Hi, how are you?'
+
+console.log(message);
+
+const isComing = confirm('Будь-ласка, підтвердіть ваше замовлення');
+console.log(isComing);
