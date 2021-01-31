4a7a70481622925e8ee7426652b67becd435d8fb
Aleksandra Ivanets <aleksandraivanets@iMac-Aleksandra.local>
2021-01-31 20:02:37 +0200

    homework_JS-02

---
+++ homework_JS-02/js/script.js
@@ -0,0 +1,91 @@
+'use strict';
+
+//----------------part 1
+
+const name = prompt('Ведіть своє імя');
+
+//----------------part 2
+
+const myAge = 14;
+const friendAge = 13;
+
+console.log(myAge + friendAge);
+
+alert('27');
+
+//----------------part 3
+
+const a = 10;
+const b = 5;
+
+console.log(a * b);
+
+
+//----------------part 4
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+
+console.log(a1);
+
+console.log(a2);
+
+console.log(a3);
+
+console.log(a4);
+
+
+//---------------part 5
+
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+console.log(a6);
+alert(a6);
+
+console.log(a7);
+alert(a7);
+
+console.log(a8);
+alert(a8);
+
+console.log(a9);
+alert(a9);
+
+console.log(a10);
+alert(a10);
+
+
+//--------------part 6
+
+let height = 23;
+let width = 10;
+
+
+console.log('S :' + height * width + 'см');
+
+//--------------part 7
+
+let heightC = 10;
+let dC = 4;
+let p = 3.14;
+
+console.log('V :' + p * dC * dC * heightC);
+
+//--------------part 8
+
+let n = 3;
+let m = 4;
+
+const result = Math.pow(3, 2);
+console.log(result);
+
+const resulT = Math.pow(4, 2);
+console.log(resulT);
+
+console.log('k :' + (result + resulT) / 5);
