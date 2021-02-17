a0746caa1d74ef91decbcf7a31e75c13e2cbb583
aleksandraivanets <aleksandraivanets@gmail.com>
2021-02-17 16:51:52 +0200

    homework_JS-04

--- homework_JS-04/js/script.js
+++ homework_JS-04/js/script.js
@@ -2,15 +2,19 @@
 
 //--------PART 1
 
+const num = prompt('Введіть число');
 
+if (num) {
+    if ((!Number.isNaN(1 * num)) && (num >= 55) && (num <= 99)) {
+        console.log('Число попадає в діапазон');
+    }
 
+    else {
+        console.log('Число не попадає в діапазон');
+    }
+}
 
-
-
-
-
-
-
+console.log('--------------------');
 
 
 
@@ -38,22 +42,57 @@ console.log(age2);
 
 //----------PART 3
 
-let name = 'Олександра';
-let surname = 'Іванець';
+const name = 'Олександра';
+const surname = 'Іванець';
+const something = 'УПС';
+let fullName;
 
 console.log(name.length);
 console.log(surname.length);
 
-const fullNum = name + surname;
+let name1 = 10;
+let name2 = 7;
 
-console.log(fullNum.length);
-
-if (name, surname) {
-    if ((!Number.isNaN(1 * name.length, surname.length)) && (name.length, surname.length > 0)) {
-        if (name.length < 4, surname.length < 5) {
-            console.log(fullNum.length);
-        } else if (name.length < 4, surname.length < 5) {
-            console.log('УПС');
-        }
-    }
+if (name1 > 4 && name2 > 5) {
+    fullName = name1 + name2;
+} else {
+    fullName = something;
 }
+
+console.log(fullName);
+
+//----------PART 4
+
+let num1 = Math.random() * (5 - 1) + 1;
+
+if ((num1 < 5) && (num1 > 1)) {
+    console.log(num1);
+}
+
+
+//---------PART 5
+
+let lang = prompt('Оберіть мову => "ua, en, ru, fr"');
+lang = lang ? lang.toLowerCase() : false;
+
+switch (lang) {
+    case 'ua':
+        console.log('Січень');
+        break;
+
+    case 'en':
+        console.log('January');
+        break;
+
+    case 'ru':
+        console.log('Январь');
+        break;
+
+    case 'fr':
+        console.log('Janvier');
+        break;
+
+    default:
+        console.log('Вашої мови немає в списку');
+}
+
