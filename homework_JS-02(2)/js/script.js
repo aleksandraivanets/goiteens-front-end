4791d526f1bcaae2f042e8f319ab61b819b7832f
aleksandraivanets <aleksandraivanets@gmail.com>
2021-04-04 20:39:09 +0300

    homework_JS-02(2)

---
+++ homework_JS-02(2)/js/script.js
@@ -0,0 +1,75 @@
+'use strict'
+
+
+console.log('-----------PART1-------------');
+
+const hello1 = function () {
+    console.log('Привіт JavaScript');
+};
+
+hello1();
+
+
+
+console.log('-----------PART2-------------');
+
+//*const name = 'Василь';
+//const name2 = 'Привіт, Василь!'
+
+function hello2(name = prompt('Ведіть Ваше ім"я')) {
+    console.log(`Привіт, ${name}!`);
+}
+
+hello2();
+
+console.log('-----------PART3-------------');
+
+const mul = function (n, m) {
+    return `n * m = ${n * m}, n + m = ${n + m}, n - m = ${n - m}`
+}
+
+console.log(mul(5, 9));
+
+
+console.log('-----------PART4-------------');
+
+const myNewArrowFunction = (...rest) => {
+    console.log(rest);
+};
+
+myNewArrowFunction(1, 2, 3);
+myNewArrowFunction(100, 200, 300, 400, 500);
+myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
+
+
+console.log('-----------PART5-------------');
+
+const mark = [100, 75, 81, 96]
+
+const myAverageScore = function (...mark) {
+    let totalMark = 0;
+    let lastMark;
+    for (const total of arguments) {
+        totalMark += total;
+    };
+
+    lastMark = totalMark / mark.length;
+    if (lastMark <= 100 && lastMark >= 91) {
+        console.log('My average score: A');
+    } else if (lastMark <= 90 && lastMark >= 81) {
+        console.log('My average score: B');
+    } else if (lastMark <= 80 && lastMark >= 71) {
+        console.log('My average score: C');
+    } else if (lastMark <= 70) {
+        console.log('My average score: D');
+    };
+};
+
+myAverageScore(...mark);
+
+
+
+
+
+
+
