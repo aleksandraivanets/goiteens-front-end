5652d45334ce8c97279a44dfeba3e4170fe66588
aleksandraivanets <aleksandraivanets@gmail.com>
2021-04-12 14:06:41 +0300

    homework_JS-01(2.)

--- homework_JS-01(2)/js/script.js
+++ homework_JS-01(2)/js/script.js
@@ -1,52 +1,54 @@
-'use strict';
+"use strict";
 
 //----------------part 1
 
-console.log('-------------PART1--------------')
+console.log("-------------PART1--------------");
 
-const Arr1 = [1, 5, '4', 'hello'];
+const Arr1 = [1, 5, "4", "hello"];
 console.log(Arr1[1]);
 
-const Arr2 = [true, 2, '{}', ['a'], 222];
+const Arr2 = [true, 2, "{}", ["a"], 222];
 console.log(Arr2[1]);
 
 console.log(Arr1[1] + Arr2[1]);
 
-console.log('------------PART2---------------')
+console.log("------------PART2---------------");
 
-Arr1.push(22);
+Arr1[4] = 22;
 console.log(Arr1);
 
-console.log('------------PART3---------------')
+//  Arr1.push(22);
+//  console.log(Arr1);
+
+console.log("------------PART3---------------");
 
 for (const Arr22 of Arr2) {
-    console.log(Arr22);
+  console.log(Arr22);
 }
 
-console.log('------------PART4---------------')
+console.log("------------PART4---------------");
 
-let message = 'Welcome to Ukraine!';
+let message = "Welcome to Ukraine!";
 
-message = message.split('')
+message = message.split("");
 console.log(message);
 
-console.log(message.indexOf('l'));
+console.log(message.indexOf("l"));
 
-console.log(message.join(''));
+console.log(message.join(""));
 
-console.log('------------PART5---------------')
+console.log("------------PART5---------------");
 
-const styles = ['Джаз', 'Блюз'];
+const styles = ["Джаз", "Блюз"];
 
-styles.push('Рок-н-ролл');
+styles.push("Рок-н-ролл");
 
-styles.splice(1, 1, 'Класика');
+styles.splice(1, 1, "Класика");
 
 const styles1 = styles.splice(0, 1);
 
 console.log(styles1);
 
-styles.unshift('Реп', 'Реггі');
+styles.unshift("Реп", "Реггі");
 
 console.log(styles);
-
